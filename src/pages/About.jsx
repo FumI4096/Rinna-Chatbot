0539facde070a7e70387c68dca315bec3730aef4
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-06-21 22:02:23 +0800

    Added Article component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Article from '../components/Article'
+import Footer from "../components/Footer"
 
 function About() {
     return (
         <>
-            <div>About</div>
+            <main>
+                <Article 
+                    title="Colegio de San Juan de Letran Calamba" 
+                    comment="Some message"
+                    image=""
+                />
+                <Article
+                    title="School of Computer Studies and Technology" 
+                    comment="Some message"
+                    image=""
+                />
+                <Article 
+                    title="Credits to Rinna Developers"
+                    comment="some message"
+                    image=''
+                />
+            </main>
+
+            <Footer />
         </>
     
     )
