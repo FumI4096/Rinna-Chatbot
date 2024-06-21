a84ae583ed90619a1931e667b36597792d2c67a3
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-06-21 22:02:59 +0800

    Made some modification to the article components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Article from '../components/Article'
+import Footer from '../components/Footer'
 
 function Home() {
     return (
@@ -11,28 +12,32 @@ function Home() {
                 <button>Give us your Comment</button>
             </header>
 
-            
-            <section>
-                <Article 
-                    title="Colegio de San Juan de Letran Calamba" 
-                    comment="Some message"
-                />
-                <Article
-                    title="School of Computer Studies and Technology" 
-                    comment="Some message"
-                />
-            </section>
+            {/*Main secction*/}
+            <main>
+                <section>
+                    <Article 
+                        title="Colegio de San Juan de Letran Calamba" 
+                        comment="Some message"
+                        image=''
+                    />
+                    <Article
+                        title="School of Computer Studies and Technology" 
+                        comment="Some message"
+                        image=''
+                    />
 
-            <section>
-            <Article 
-                    title="What is Artificial Intelligence?" 
-                    comment="Some message"
-                />
-                <Article
-                    title="Who is Rinna?" 
-                    comment="Some message"
-                />
-            </section>
+                    <Article
+                        title="Get a Chitchat with Rinna" 
+                        comment="Some message"
+                        image=''
+                        showButton={true}                   
+                    />
+                </section>
+            </main>
+
+            {/*Footer section*/}
+            <Footer />
+
         </>
 
     )
