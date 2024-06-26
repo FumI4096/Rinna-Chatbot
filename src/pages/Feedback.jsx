2be7ba56eefa6b8193bf61bd8a7cadc3d1c52a1f
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-06-26 15:17:27 +0800

    Added forms

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function Feedback() {
     return (
-        <div>Feedback</div>
+        <>
+            <h2>We want to know your thoughts!</h2>
+            <div>
+                <form action="">
+                    <article>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id='name'/>
+                    </article>
+                    <article>
+                        <label htmlFor="">Email:</label>
+                        <input type="text" id='email'/> 
+                    </article>
+                    <article>
+                        <label htmlFor="message">Message:</label>
+                        <textarea name="" id="message"></textarea> 
+                    </article>
+
+                </form> 
+            </div>
+        </>
+
     )
 }
 
