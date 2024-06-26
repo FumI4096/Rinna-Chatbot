19f0fbe92ff6e63602308586e6caf788d2edce88
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-06-26 15:17:12 +0800

    Added components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import FooterAreas from './FooterAreas'
 
 function Footer() {
   return (
     <>
+      <h2>Get to know more about them!</h2>
       <footer>
-        <h2>Get to know more about them</h2>
+        <FooterAreas 
+          title='Colegio de San Juan de Letran Calamba'
+          link='https://www.facebook.com/LetranCalambaOfficial'
+        />
+        <FooterAreas 
+          title='School of Computer Studies and Technology'
+          link='https://www.facebook.com/SCSTSCofficial'
+        />
+        <FooterAreas 
+          title='Rinna Developers'
+          link='https://developers.rinna.co.jp/'
+        />
+
+
       </footer>
     </>
   )
