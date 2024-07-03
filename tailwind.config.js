1184dc2818971210e1440cbd976abf7b1de832f5
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-07-03 20:17:17 +0800

    made more extension and adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,22 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'headerBackground': "url('../images/BG5.jpg')",
+        'heroBackgroundOne': "url('../images/BG1.jpg')",
+        'heroBackgroundTwo': "url('../images/BG5.jpg')"
       },
       height: {
         '1xl': '30rem',
         '2xl': '35rem',
+      },
+      fontSize: {
+        'base': '1rem',
+      },
+      backgroundColor: {
+        'gray1': 'rgb(166,166,166)',
+        'violet-custom-1': '#383838',
+        'gray-custom-1': '#181818'
       }
+
     },
   },
   plugins: [],
