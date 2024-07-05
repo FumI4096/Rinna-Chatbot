2b71dd41270a24cfda31ae31cbc91d69ebaa1747
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-07-05 21:45:10 +0800

    Resolved merge conflict in FooterAreas.jsx

--- src/components/FooterAreas.jsx
+++ src/components/FooterAreas.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 
-const FooterAreas = ({image, imageAlt, link, title}) => {
+<<<<<<< Updated upstream
+const FooterAreas = (image, imageAlt, link, title) => {
     return (
         <article>
             <img src={image} alt={imageAlt}/>
@@ -10,13 +11,40 @@ const FooterAreas = ({image, imageAlt, link, title}) => {
         
         </article>
     )
+=======
+const FooterAreas = ({imageExist, iconExist, icon, image, imageAlt, link, title}) => {
+    if(imageExist){
+        return(
+            <>
+                <li className='h-12 flex items-center gap-4'>
+                    <img src={image} alt={imageAlt} className='h-full rounded-full'/>
+                    <a href={link} target='_blank' rel='noreferrer'>{title}</a>
+                    
+                </li>
+            </>
+        )
+    }
+    else if(iconExist){
+        return(
+            <li><a href={link} target='_blank' rel='noreferrer'>{icon}</a></li>
+        )
+    }
+>>>>>>> Stashed changes
 }
 
 FooterAreas.propTypes = {
+    imageExist: PropTypes.bool,
+    iconExist: PropTypes.bool,
+    icon: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    imageAlt: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired
+    link: PropTypes.string.isRequired,
+
+}
+
+FooterAreas.defaultPrps = {
+    imageExist: false,
+    iconExist: false
 }
 
 export default FooterAreas
