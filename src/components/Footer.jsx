5072ed193ba4aacccab03268ed6c23c87876246a
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-07-11 18:42:09 +0800

    Added react-icons, structure, components and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,43 @@ import FooterAreas from './FooterAreas'
 import Letran from '../images/letran-logo.jpg'
 import SCST from '../images/scst-logo.jpg'
 import Rinna from '../images/rinna.png'
-// import { FaFacebook, FaInstagram, FaXTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
     <>
-      <footer className='bg-violet-500 px-24 py-16 h-80 flex justify-between items-center flex-row relative'>
+      <footer className='bg-violet-600 px-24 py-16 h-96 flex justify-between items-center flex-row relative'>
         <section className='w-full flex justify-center flex-col items-start'>
-          
-          <h1 className='font-anton text-5xl'>PRINCY</h1>
-          <p>A model for introduction in artificial intelligence and <br />application programming interface</p>
+          <h1 className='text-white tracking-custom-spacing-1 font-anton text-5xl mb-3 drop-shadow-xl'>PRINCY</h1>
+          <p className='text-violet-950'>A model for introduction in artificial intelligence and <br />application programming interface</p>
         </section>
-        <section className='h-full w-full flex justify-center items-center flex-col'>
-          <h2>Get to know more about them!</h2>
-          <article>
-            <ul className='w-full flex flex-col gap-3'>
+        <section className='h-full w-full flex flex-col items-center'>
+          <h2 className='text-3xl text-white font-bold drop-shadow-lg'>Princy's Socials</h2>
+          <article className='w-full h-full flex justify-center items-center'>
+            <ul className='flex flex-col gap-3'>
+              <FooterAreas 
+                iconExist={true}
+                icon={<FaFacebook className='text-2xl'/>}
+                title="Princy AI"
+              />
+              <FooterAreas 
+                iconExist={true}
+                icon={<FaInstagram className='text-2xl'/>}
+                title="Princy"
+              />
+              <FooterAreas 
+                iconExist={true}
+                icon={<FaXTwitter className='text-2xl'/>}
+                title="PrincyRinnaAI"
+              />
+            </ul>
+          </article>
+        </section>
+        <section className='h-full w-full flex items-center flex-col'>
+          <h2 className='text-3xl font-bold text-white drop-shadow-lg'>Get to know more about them!</h2>
+          <article className='h-full flex justify-center items-center w-full'>
+            <ul className='w-auto flex flex-col gap-3'>
               <FooterAreas 
                 imageExist={true}
                 image={Letran}
@@ -41,7 +63,7 @@ function Footer() {
 
         </section>
 
-        <p className='absolute bottom-0 left-0 pl-24 bg-violet-700 w-full py-1 font-mono'>&#169; 2024 Princy Bot. All rights reserved.</p>
+        <p className='absolute bottom-0 left-0 pl-24 bg-violet-800 w-full py-1 font-mono'>&#169; 2024 Princy Bot. All rights reserved.</p>
 
 
 
