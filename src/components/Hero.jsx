67ed8c7dde96698a8fda10fd5cfd1a9da75ee5d3
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-07-16 17:45:06 +0800

    Made adjustment on styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-const Hero = ({buttonExist, buttonOne, buttonTwo, sentence}) => {
+const Hero = ({buttonExist, buttonOne, buttonTwo, phrase, phraseExist}) => {
     return (
         <>
             <header className={`relative after:content-[""] after:absolute after:inset-0 after:bg-neutral-950/70 isolation ${
-                (sentence === "#IChooseLetran") ? 'bg-heroBackgroundOne' : 'bg-heroBackgroundTwo'
+                (phrase === "Innovating Education, Empowering Futures") ? 'bg-heroBackgroundOne' : 'bg-heroBackgroundTwo'
             } bg-no-repeat bg-cover h-2xl flex items-center justify-center gap-3`}>
-                <p>{sentence}</p>
+                <p className='text-5xl text-white z-10  font-anton  tracking-custom-spacing-1'>{phrase}</p>
                 { buttonExist && (      
                     <>
                         <div className='h-auto w-auto z-10'><Link to='/chat' className='bg-violet-600 text-white text-base font-bold  flex items-center justify-center rounded-full h-14 w-64'>{buttonOne}</Link></div>
@@ -22,14 +22,16 @@ const Hero = ({buttonExist, buttonOne, buttonTwo, sentence}) => {
 
 Hero.propTypes = {
     buttonExist: PropTypes.bool,
+    phraseExist: PropTypes.bool,
     buttonOne: PropTypes.string.isRequired,
     buttonTwo: PropTypes.string.isRequired,
-    sentence: PropTypes.string.isRequired
+    phrase: PropTypes.string.isRequired
 }
 
 Hero.defaultProps = {
-    buttonExist: true,
-    sentence: ''
+    buttonExist: false,
+    phraseExist: false,
+    phrase: ''
 }
 
 export default Hero
