ca66ede3a9adc79cbc0ad64b5abfb45be11e7cda
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-07-16 17:41:31 +0800

    Added extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'heroBackgroundOne': "url('../images/BG1.jpg')",
-        'heroBackgroundTwo': "url('../images/BG5.jpg')"
+        'heroBackgroundTwo': "url('../images/BG5.jpg')",
       },
       height: {
         '1xl': '30rem',
@@ -20,7 +20,7 @@ module.exports = {
       backgroundColor: {
         'gray1': 'rgb(166,166,166)',
         'violet-custom-1': '#383838',
-        'gray-custom-1': '#181818'
+        'gray-custom-1': '#181818',
       },
       fontFamily: {
         'anton': ["Anton SC", "sans-serif"],
@@ -31,6 +31,11 @@ module.exports = {
       },
       letterSpacing: {
         'custom-spacing-1': '0.25rem'
+      },
+      colors: {
+        'gray-custom-2': 'rgb(60, 60, 60)',
+        'gray-custom-3': 'rgb(70, 70, 70)',
+        'gray-custom-4': 'rgb(100, 100, 100)'
       },
     }
   },
