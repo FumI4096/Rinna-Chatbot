e1c909148c0a12974e551a3db9de963f3982e530
Seimon Maiquez <Sesmaiquez@gmail.com>
2024-07-22 09:34:11 +0800

    Change from flexbox layout to grid layout. Added responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,13 @@ import { FaXTwitter } from "react-icons/fa6";
 function Footer() {
   return (
     <>
-      <footer className='bg-violet-600 px-24 py-16 h-96 flex justify-between items-center flex-row relative'>
-        <section className='w-full flex justify-center flex-col items-start'>
-          <h1 className='text-white tracking-custom-spacing-1 font-anton text-5xl mb-3 drop-shadow-xl'>PRINCY</h1>
-          <p className='text-violet-950'>A model for introduction in artificial intelligence and <br />application programming interface</p>
+      <footer className='bg-violet-600 px-24 py-24 h-auto  relative grid grid-cols-3 gap-5 auto-cols-auto max-xl:grid-cols-2 max-xl:gap-9 max-md:grid-cols-1 max-md:gap-11'>
+        <section className='w-full flex-grow-0 flex justify-center flex-col items-start gap-3 max-md:items-center'>
+          <h1 className='text-white tracking-custom-spacing-1 font-anton text-5xl drop-shadow-xl'>PRINCY</h1>
+          <p className='text-violet-950 max-md:text-center'>A model for introduction in artificial intelligence and application programming interface</p>
         </section>
-        <section className='h-full w-full flex flex-col items-center'>
-          <h2 className='text-3xl text-white font-bold drop-shadow-lg'>Princy's Socials</h2>
+        <section className='h-full w-full flex flex-col items-center gap-3'>
+          <h2 className='text-2xl text-white font-bold drop-shadow-lg'>Princy's Socials</h2>
           <article className='w-full h-full flex justify-center items-center'>
             <ul className='flex flex-col gap-3'>
               <FooterAreas 
@@ -36,10 +36,10 @@ function Footer() {
             </ul>
           </article>
         </section>
-        <section className='h-full w-full flex items-center flex-col'>
-          <h2 className='text-3xl font-bold text-white drop-shadow-lg'>Get to know more about them!</h2>
-          <article className='h-full flex justify-center items-center w-full'>
-            <ul className='w-auto flex flex-col gap-3'>
+        <section className='h-full w-full flex items-center flex-col gap-3 max-xl:col-span-2 max-xl:items-start max-md:col-span-1 max-md:items-center'>
+          <h2 className='text-2xl font-bold text-white drop-shadow-lg'>Know more about them!</h2>
+          <article className='h-full flex justify-center items-center w-full max-xl:justify-normal max-md:justify-center'>
+            <ul className='w-auto flex flex-col gap-6 max-xl:items-start'>
               <FooterAreas 
                 imageExist={true}
                 image={Letran}
